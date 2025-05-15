228d6e0689b04d3588aba1bfee0b8721edbe1de9
OscarJEB <burgeo1@macstudent.com.au>
2025-05-15 23:10:15 +0000

    started student class

---
+++ Main.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, firstName, lastName)
+    self.firstName = firstName
+    self.lastName = lastName
+
+    #print full name method
+    def printFullName(self):
+        print(f(self.firstName, self.lastName))
+
+    #student class
+    class Student:
+        def __init__(self, firstName, lastName, studentID, houseG)
+            super __init__(firstName, lastName)
+            self.studentID = studentID
+            self.houseG = houseG
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Write up an explanation on the design of the class diagram
+The class diagram shows a person class with only attributes "first name" and "last name", and a method "Print full name".
+The parent and student classes both inherit from the person class, with the parent class having occupation and boolean methods.
+The student class, however, have a homeroom and unique student ID attributes, and they can be enrolled in subjects.
